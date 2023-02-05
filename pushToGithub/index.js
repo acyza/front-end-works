c28463b8c05f538739cae7ebbcba490ff656b32d
acyza <101238421+acyza@users.noreply.github.com>
2023-02-05 19:23:53 +0800

    update Sun Feb 05 2023

--- pushToGithub/index.js
+++ pushToGithub/index.js
@@ -1,5 +1,7 @@
 const {writeFile, readdir,stat} = require("fs/promises")
 const { spawnSync } = require("child_process")
+const Selenium = require("selenium-webdriver")
+const Firefox = require("selenium-webdriver/firefox")
 
 async function isDir(name) {
   return (await stat(name)).isDirectory()
@@ -13,12 +15,35 @@ function exec(name){
   }
 }
 
+let driver;
+
+async function screenshot(dir){
+  if(!driver){
+    const builder = new Selenium.Builder();
+    builder.forBrowser(Selenium.Browser.FIREFOX)
+    builder.setFirefoxOptions(
+      new Firefox.Options()
+      .windowSize({
+        width: 500,
+        height: 500,
+      })
+    );
+    driver = builder.build();
+  }
+  await driver.get(`file://${__dirname}/../${dir}/index.html`);
+  driver
+  const png = await driver.takeScreenshot();
+  await writeFile(`${dir}/screenshot.png`, Buffer.from(png,"base64"));
+}
+
 async function main(){
+
   const dirs = await readdir("./")
   let data = "const list=["
   const exclude = ["previewServer","pushToGithub",".git",".github"]
   for(const dir of dirs) {
     if(exclude.includes(dir) || !await isDir(dir))continue;
+    await screenshot(dir);
     data = `${data}\"${dir}\",`
   }
   await writeFile("list.js",data+"]")
